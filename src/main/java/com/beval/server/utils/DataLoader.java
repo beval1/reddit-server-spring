6b51d6899cafe4f2b78e390b43cfdd6d29e733ec
beval1 <bobo91280@gmail.com>
2022-08-19 19:52:49 +0300

    PROD profile updated - changed database initialization from create-drop to update

--- src/main/java/com/beval/server/utils/DataLoader.java
+++ src/main/java/com/beval/server/utils/DataLoader.java
@@ -33,167 +33,168 @@ public class DataLoader implements ApplicationRunner {
 
     @Transactional
     public void run(ApplicationArguments args) {
-        //set user roles
-        RoleEntity adminRole = roleRepository.save(RoleEntity.builder().roleName(RoleEnum.ADMIN).build());
-        RoleEntity userRole = roleRepository.save(RoleEntity.builder().roleName(RoleEnum.USER).build());
+        if (roleRepository.count() == 0) {
+            //set user roles
+            RoleEntity adminRole = roleRepository.save(RoleEntity.builder().roleName(RoleEnum.ADMIN).build());
+            RoleEntity userRole = roleRepository.save(RoleEntity.builder().roleName(RoleEnum.USER).build());
 
-        UserEntity deletedUser = userRepository.save(
-                UserEntity
-                        .builder()
-                        .username("deleted")
-                        .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
-                        .enabled(false)
-                        .roles(Set.of(userRole))
-                        .birthdate(null)
-                        .firstName(null)
-                        .lastName(null)
-                        .email("deleted@deleted.com")
-                        .build()
-        );
+            UserEntity deletedUser = userRepository.save(
+                    UserEntity
+                            .builder()
+                            .username("deleted")
+                            .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
+                            .enabled(false)
+                            .roles(Set.of(userRole))
+                            .birthdate(null)
+                            .firstName(null)
+                            .lastName(null)
+                            .email("deleted@deleted.com")
+                            .build()
+            );
 
-        UserEntity user = userRepository.save(
-                UserEntity
-                        .builder()
-                        .username("test1")
-                        .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
-                        .enabled(true)
-                        .roles(Set.of(userRole))
-                        .birthdate(null)
-                        .firstName("Test")
-                        .lastName("Test")
-                        .email("test@test.com")
-                        .build()
-        );
+            UserEntity user = userRepository.save(
+                    UserEntity
+                            .builder()
+                            .username("test1")
+                            .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
+                            .enabled(true)
+                            .roles(Set.of(userRole))
+                            .birthdate(null)
+                            .firstName("Test")
+                            .lastName("Test")
+                            .email("test@test.com")
+                            .build()
+            );
 
-        userRepository.save(
-                UserEntity
-                        .builder()
-                        .username("admin")
-                        .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
-                        .enabled(true)
-                        .roles(Set.of(adminRole))
-                        .birthdate(null)
-                        .firstName("Admin")
-                        .lastName("Adminov")
-                        .email("admin@admin.com")
-                        .build()
-        );
+            userRepository.save(
+                    UserEntity
+                            .builder()
+                            .username("admin")
+                            .password("$2a$12$w.GNfFrtuRMFSxWq0TZsgO2M/O3jTwZ8cvdL3X/EW0XQKNitCqD6K")
+                            .enabled(true)
+                            .roles(Set.of(adminRole))
+                            .birthdate(null)
+                            .firstName("Admin")
+                            .lastName("Adminov")
+                            .email("admin@admin.com")
+                            .build()
+            );
 
-        SubredditEntity subreddit = subredditRepository.save(
-                SubredditEntity
-                        .builder()
-                        .moderators(Set.of(user))
-                        .name("SubredditTitle")
-                        .description("tralalalallalallalallalallalalalalalallalalallalalal")
-                        .build()
-        );
+            SubredditEntity subreddit = subredditRepository.save(
+                    SubredditEntity
+                            .builder()
+                            .moderators(Set.of(user))
+                            .name("SubredditTitle")
+                            .description("tralalalallalallalallalallalalalalalallalalallalalal")
+                            .build()
+            );
 
-        //Add member
-        user.getSubreddits().add(subreddit);
-        subreddit.getMembers().add(user);
+            //Add member
+            user.getSubreddits().add(subreddit);
+            subreddit.getMembers().add(user);
 
-        PostEntity post = postRepository.save(
-            PostEntity
-                    .builder()
-                    .title("Testov post1111")
-                    .author(user)
-                    .subreddit(subreddit)
-                    .type("text")
-                    .build()
-        );
+            PostEntity post = postRepository.save(
+                    PostEntity
+                            .builder()
+                            .title("Testov post1111")
+                            .author(user)
+                            .subreddit(subreddit)
+                            .type("text")
+                            .build()
+            );
 
-        PostEntity post2 = postRepository.save(
-                PostEntity
-                        .builder()
-                        .title("vtori testov post")
-                        .author(user)
-                        .subreddit(subreddit)
-                        .type("image")
-                        .upvotedUsers(Set.of(user))
-                        .build()
-        );
-        post2.setCreatedOn(LocalDateTime.of(2020, 1, 1, 1, 1));
+            PostEntity post2 = postRepository.save(
+                    PostEntity
+                            .builder()
+                            .title("vtori testov post")
+                            .author(user)
+                            .subreddit(subreddit)
+                            .type("image")
+                            .upvotedUsers(Set.of(user))
+                            .build()
+            );
+            post2.setCreatedOn(LocalDateTime.of(2020, 1, 1, 1, 1));
 
-        PostEntity post3 = postRepository.save(
-                PostEntity
-                        .builder()
-                        .title("Link post newwwwwwwwwwwwww")
-                        .author(user)
-                        .subreddit(subreddit)
-                        .type("link")
-                        .build()
-        );
-        post3.setCreatedOn(LocalDateTime.of(2022, 6, 1, 1, 1));
-
-
-        PostEntity post4 = postRepository.save(
-                PostEntity
-                        .builder()
-                        .title("Image post newwwwwwwwwwwwww")
-                        .author(user)
-                        .subreddit(subreddit)
-                        .type("image")
-                        .build()
-        );
+            PostEntity post3 = postRepository.save(
+                    PostEntity
+                            .builder()
+                            .title("Link post newwwwwwwwwwwwww")
+                            .author(user)
+                            .subreddit(subreddit)
+                            .type("link")
+                            .build()
+            );
+            post3.setCreatedOn(LocalDateTime.of(2022, 6, 1, 1, 1));
 
 
-        CommentEntity commentEntity = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post)
-                        .content("lalalalal")
-                        .author(user)
-                        .parentComment(null)
-                        .build()
-        );
-        CommentEntity comment2 = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post2)
-                        .content("https://preview.redd.it/o9hc71py7gh91.jpg?width=640&crop=smart&auto=webp&s=6b47a90dfb2031707bf34dc67e0975eb06ab2158")
-                        .author(user)
-                        .parentComment(null)
-                        .build()
-        );
-        CommentEntity comment3 = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post3)
-                        .content("https://www.w3schools.com/w3css/img_lights.jpg")
-                        .author(user)
-                        .parentComment(null)
-                        .build()
-        );
+            PostEntity post4 = postRepository.save(
+                    PostEntity
+                            .builder()
+                            .title("Image post newwwwwwwwwwwwww")
+                            .author(user)
+                            .subreddit(subreddit)
+                            .type("image")
+                            .build()
+            );
 
-        CommentEntity comment4 = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post4)
-                        .content("https://i.redd.it/c3r6krnwdch91.png")
-                        .author(user)
-                        .parentComment(null)
-                        .build()
-        );
 
-        CommentEntity reply1 = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post)
-                        .content("22222222222222")
-                        .author(user)
-                        .parentComment(commentEntity)
-                        .build()
-        );
+            CommentEntity commentEntity = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post)
+                            .content("lalalalal")
+                            .author(user)
+                            .parentComment(null)
+                            .build()
+            );
+            CommentEntity comment2 = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post2)
+                            .content("https://preview.redd.it/o9hc71py7gh91.jpg?width=640&crop=smart&auto=webp&s=6b47a90dfb2031707bf34dc67e0975eb06ab2158")
+                            .author(user)
+                            .parentComment(null)
+                            .build()
+            );
+            CommentEntity comment3 = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post3)
+                            .content("https://www.w3schools.com/w3css/img_lights.jpg")
+                            .author(user)
+                            .parentComment(null)
+                            .build()
+            );
 
-        CommentEntity replyToReply = commentRepository.save(
-                CommentEntity
-                        .builder()
-                        .post(post)
-                        .content("3333333333333333333")
-                        .author(user)
-                        .parentComment(reply1)
-                        .build()
-        );
+            CommentEntity comment4 = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post4)
+                            .content("https://i.redd.it/c3r6krnwdch91.png")
+                            .author(user)
+                            .parentComment(null)
+                            .build()
+            );
 
+            CommentEntity reply1 = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post)
+                            .content("22222222222222")
+                            .author(user)
+                            .parentComment(commentEntity)
+                            .build()
+            );
+
+            CommentEntity replyToReply = commentRepository.save(
+                    CommentEntity
+                            .builder()
+                            .post(post)
+                            .content("3333333333333333333")
+                            .author(user)
+                            .parentComment(reply1)
+                            .build()
+            );
+        }
     }
 }
